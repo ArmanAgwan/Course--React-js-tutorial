856f8a4540775f0b71361cb4e488003b86ac7e41
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2025-07-22 20:07:29 +0530

    Add files via upload

---
+++ ReactHook.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import LifecycleA from "./LifecycleA";
+import Form from "./Form";
+import ParentComp from "./ParentComp";
+import RefsDemo from "./RefsDemo";
+import FocusInput from "./FocusInput";
+import FRParentInput from "./FRParentInput";
+import PortalDemo from "./PortalDemo";
+import Hero from "./Hero";
+import ErrorBoundary from "./ErrorBoundary";
+import ClickCounter from "./ClickCounter";
+import HoverCounter from "./HoverCounter";
+import ClickCounterTwo from "./ClickCounterTwo";
+import HoverCounterTwo from "./HoverCounterTwo";
+
+class ReactHook extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <ErrorBoundary> */}
+        {/* <Hero heroName="Batman" />
+              <Hero heroName="Superman" />
+              <Hero heroName="Joker" /> */}
+        {/* </ErrorBoundary>   */}
+        <ClickCounterTwo />
+        <HoverCounterTwo />
+        {/* <ClickCounter /> */}
+        {/* <HoverCounter /> */}
+        {/* <ClickCounterTwo /> */}
+        {/* <PortalDemo /> */}
+        {/* <FRParentInput /> */}
+        {/* <FocusInput /> */}
+        {/* <RefsDemo /> */}
+        {/* <ParentComp /> */}
+        {/* <Form /> */}
+        {/* <LifecycleA /> */}
+        {/* <Table /> */}
+        {/* <FragmentDemo /> */}
+      </div>
+    );
+  }
+}
+
+export default ReactHook;
